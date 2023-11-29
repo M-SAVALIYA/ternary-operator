193f1b3fb1c97cf73987908f8f6b085238c64649
M-SAVALIYA <152249677+M-SAVALIYA@users.noreply.github.com>
2023-11-29 08:22:57 -0800

    Add files via upload

---
+++ ternary-2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+main()
+{
+	int  a,b,c;
+	printf("Enter value A = ");
+	scanf("%d",&a);
+	printf("Enter value B = ");
+	scanf("%d",&b);
+	
+	(a>b) ? printf("A is max") : printf("B is max");
+
+}
